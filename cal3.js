e89165154812b892059d47e7106caeb367201e1f
Faaiz1141 <faaizfaaiz521@gmail.com>
2024-09-21 09:05:12 +0500

    Add files via upload

---
+++ cal3.js
@@ -0,0 +1,22 @@
+//Calculation Program
+const display = document.getElementById('display');
+
+function appendToDisplay(input){
+    display.value += input;
+
+}
+ 
+function cleardisplay(){
+
+    display.value = "";
+}
+
+function calculate(){
+    try{
+    display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+
+}
